fa72b800c3b6387fe35fb0a26c819eea9a9b4758
AlaaAziz2020 <87227043+AlaaAziz2020@users.noreply.github.com>
2025-05-18 00:08:51 +0300

    Update Layout.jsx

--- Frontend/src/component/Layout/Layout.jsx
+++ Frontend/src/component/Layout/Layout.jsx
@@ -3,7 +3,7 @@ import Navbar from '../Navbar/Navbar';
 import { Outlet } from 'react-router';
 import Footer from '../Footer/Footer'
 
-export default function L() {
+export default function Layout() {
   return (
     <div>
         <Navbar />
